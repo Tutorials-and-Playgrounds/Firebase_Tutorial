43eb1d8bd9ae271c5e0544533ab6a625e90fe874
Joshua Meiser <jm@joshuameiser.de>
2022-08-14 22:41:19 +0200

    added basic firebase config

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import * as firebase from "firebase/app";
+import { initializeApp } from "firebase/app";
+import "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const firebaseConfig = {
+	apiKey: "AIzaSyBPkFSNhRESZGqiIxMGM_oXkyxijkwenLE",
+	authDomain: "fir-tutorial01-f01db.firebaseapp.com",
+	projectId: "fir-tutorial01-f01db",
+	storageBucket: "fir-tutorial01-f01db.appspot.com",
+	messagingSenderId: "151513738866",
+	appId: "1:151513738866:web:e4173dede3fecf69509ceb",
+	measurementId: "G-NLQ1L784VY",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
